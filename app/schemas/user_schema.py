ac7ee5b9b4cf83d9d86cea9a3ccf9346509fdc18
bhanuka2 <bhanukashashiindra.com>
2025-11-26 23:50:40 +0530

    feat:added schema-user

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    phone: Optional[str] = None
