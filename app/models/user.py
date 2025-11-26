5f66d9428a5baab67199d8653fa23278d33d095f
bhanuka2 <bhanukashashiindra.com>
2025-11-26 23:39:50 +0530

    feat:added models-user

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy.sql import func
+
+from app.database.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    first_name = Column(String(100), nullable=True)
+    last_name = Column(String(100), nullable=True)
+    phone = Column(String(20), nullable=True)
